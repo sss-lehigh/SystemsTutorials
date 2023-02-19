c859e93d5d004029929a28ab4bec2144c074998a
dePaul Miller <depaulmillz@users.noreply.github.com>
2023-02-19 09:27:49 -0500

    Added comments

--- gpus/example2/example2.cu
+++ gpus/example2/example2.cu
@@ -15,10 +15,12 @@
 // dot product
 // we do sum a_i * b_i
 
+// Using vector type thrust::host_vector<T>
 template<typename T>
 using vector = thrust::host_vector<T>;
 
 int main() {
+    // creating our vectors x and y of size
     const int size = 1000000;
 
     vector<float> x(size, 0);
@@ -28,11 +30,13 @@ int main() {
     thrust::device_vector<float> y_device(y.size());
 
 
+    // random initialization
     for(int i = 0; i < size; ++i) {
         x[i] = rand() / static_cast<float>(RAND_MAX);
         y[i] = rand() / static_cast<float>(RAND_MAX);
     }
 
+    // sequential execution for reference, we do plus as our reduction and times as our map
     auto start = std::chrono::high_resolution_clock::now();
     float reference = std::transform_reduce(std::execution::seq, x.begin(), x.end(), y.begin(), 0.0f, [](float x, float y) {
                 return x + y;
@@ -43,6 +47,7 @@ int main() {
 
     std::cout << std::chrono::duration<double>(end - start).count() * 1e3 << " ms" << std::endl;
 
+    // execute in parallel using the C++ standard library similar to example 1
     start = std::chrono::high_resolution_clock::now();
     float val = std::transform_reduce(std::execution::par_unseq, x.begin(), x.end(), y.begin(), 0.0f, [](float x, float y) {
                 return x + y;
@@ -57,6 +62,7 @@ int main() {
 
     std::cout << std::chrono::duration<double>(end - start).count() * 1e3 << " ms" << std::endl;
 
+    // We create two lambdas and mark them as executable on both the host and the device
     auto times = [] __host__ __device__ (thrust::tuple<float, float> x) -> float {
                         return thrust::get<0>(x) * + thrust::get<1>(x);
                      };
@@ -68,12 +74,16 @@ int main() {
     
     start = std::chrono::high_resolution_clock::now();
 
+    // copy memory to the GPU
     thrust::copy(x.begin(), x.end(), x_device.begin());
     thrust::copy(y.begin(), y.end(), y_device.begin());
 
+    // create an iterator that zips x and y together, as in element one is {x[0], y[0]},
+    // element 2 is {x[1], y[1]}, and so on
     auto begin_iter = thrust::make_zip_iterator(x_device.begin(), y_device.begin());
     auto end_iter = thrust::make_zip_iterator(x_device.end(), y_device.end());
 
+    // perform the transform reduce
     val = thrust::transform_reduce(begin_iter, end_iter, times, 0.0f, plus);
     
     end = std::chrono::high_resolution_clock::now();
