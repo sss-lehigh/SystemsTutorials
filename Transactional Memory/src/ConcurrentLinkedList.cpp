aa2909907028b8630040f150b874efc4109fba3c
冀卓疌 <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2023-04-17 02:58:37 -0400

    add a “concurrent” linked list
    Basically the same as the non-concurrent linked list. There is no synchonization.

---
+++ Transactional Memory/src/ConcurrentLinkedList.cpp
@@ -0,0 +1,55 @@
+#include "ConcurrentLinkedList.h"
+
+void ConcurrentDoublyLinkedList::insertNode(
+    /* with */ int content,
+    /* afterNodeWith */ int immediatelyPrecedingContent) const {
+  report(OperationStatus::InProgress, Operation::Insert, /* content */ content, immediatelyPrecedingContent);
+  auto currentNode = head;
+  while (currentNode != nullptr &&
+         currentNode->content != immediatelyPrecedingContent) {
+    currentNode = currentNode->next;
+  }
+  if (currentNode == nullptr) {
+    report(OperationStatus::Failure, Operation::Insert, /* content */ content, immediatelyPrecedingContent);
+  } else {
+    auto newNode = new Node(content);
+    if (!currentNode->isTail()) {
+      newNode->setNext(currentNode->next);
+    }
+    newNode->setPrevious(currentNode);
+    report(OperationStatus::Success, Operation::Insert, /* content */ newNode->content, /* immediatelyPrecedingContent */ currentNode->content);
+  }
+}
+
+void ConcurrentDoublyLinkedList::appendNode(/* with */ int content) {
+  report(OperationStatus::InProgress, Operation::Append, content);
+  auto newNode = new Node(content);
+  auto tail = getTail();
+  if (tail == nullptr) {
+    head = newNode;
+  } else {
+    tail->setNext(newNode);
+  }
+  report(OperationStatus::Success, Operation::Append, newNode->content);
+}
+
+void ConcurrentDoublyLinkedList::deleteFirstNode(/* with */ int content) {
+  report(OperationStatus::InProgress, Operation::Delete, content);
+  auto currentNode = head;
+  while (currentNode != nullptr && currentNode->content != content) {
+    currentNode = currentNode->next;
+  }
+  if (currentNode == nullptr) {
+    report(OperationStatus::Failure, Operation::Delete, content);
+  } else {
+    if (currentNode->isHead()) {
+      setHead(currentNode->next);
+    } else if (currentNode->isTail()) {
+      setTail(currentNode->previous);
+    } else {
+      currentNode->previous->setNext(currentNode->next);
+    }
+    delete currentNode;
+    report(OperationStatus::Success, Operation::Delete, content);
+  }
+}
