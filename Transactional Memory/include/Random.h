7e855c0a9d09cb6efc363d102a07fda52669822c
冀卓疌 <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2023-04-24 07:38:41 -0400

    remove unused random value

--- Transactional Memory/include/Random.h
+++ Transactional Memory/include/Random.h
@@ -5,7 +5,7 @@
 
 std::random_device randomDevice;
 std::mt19937 generator(randomDevice());
-std::uniform_int_distribution<int> operationRawValueDistribution(0, 2);
+std::uniform_int_distribution<int> operationRawValueDistribution(0, 1);
 std::uniform_int_distribution<int> doubleDigitIntegerDistribution(10, 99);
 
 int randomValue() { return doubleDigitIntegerDistribution(generator); }
